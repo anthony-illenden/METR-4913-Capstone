ed2101e422a61a94bbb4a0ad26bfbf8371cc544d
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2023-10-18 18:52:34 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,11 +17,15 @@ def Read():
     local_csv = pd.read_csv('C:\\Users\\Tony\\Desktop\\Capstone\\farpod.csv')
     return local_csv
 
-def Calculate(station_list,date1,time1,date2,time2,wfo):
+def Calculate(station_list,date1,month1,year1,time1,date2,time2,month2,year2,wfo):
     website_date1 = int(date1[date1.find('/')+1:date1.find('/')+3])
     website_date2 = int(date2[date2.find('/')+1:date2.find('/')+3])
-    url = 'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station=ACB&station=ADG&station=AMN&station=ANJ&station=APN&station=ARB&station=AZO&station=BAX&station=BEH&station=BIV&station=BTL&station=CAD&station=CFS&station=CIU&station=CMX&station=CVX&station=D95&station=DET&station=DRM&station=DTW&station=DUH&station=ERY&station=ESC&station=FFX&station=FKS&station=FNT&station=FPK&station=GLR&station=GOV&station=GRR&station=HAI&station=HTL&station=HYX&station=IKW&station=IMT&station=IRS&station=ISQ&station=IWD&station=JXN&station=JYM&station=LAN&station=LDM&station=LWA&station=MBL&station=MBS&station=MCD&station=MGN&station=MKG&station=MNM&station=MOP&station=MTC&station=OEB&station=ONZ&station=OSC&station=OZW&station=P53&station=P58&station=P59&station=P75&station=PHN&station=PLN&station=PTK&station=PZQ&station=RMY&station=RNP&station=RQB&station=SAW&station=SJX&station=SLH&station=TEW&station=TTF&station=TVC&station=VLL&station=Y31&station=Y70&station=YIP&data=all&year1=2022&month1=11&day1='+str(website_date1)+'&year2=2022&month2=11&day2='+str(website_date2+1)+'&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=no&missing=M&trace=T&direct=no&report_type=3&report_type=4'
-    df_parsed = pd.read_csv(url)
+    mi_url = 'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station=ACB&station=ADG&station=AMN&station=ANJ&station=APN&station=ARB&station=AZO&station=BAX&station=BEH&station=BIV&station=BTL&station=CAD&station=CFS&station=CIU&station=CMX&station=CVX&station=D95&station=DET&station=DRM&station=DTW&station=DUH&station=ERY&station=ESC&station=FFX&station=FKS&station=FNT&station=FPK&station=GLR&station=GOV&station=GRR&station=HAI&station=HTL&station=HYX&station=IKW&station=IMT&station=IRS&station=ISQ&station=IWD&station=JXN&station=JYM&station=LAN&station=LDM&station=LWA&station=MBL&station=MBS&station=MCD&station=MGN&station=MKG&station=MNM&station=MOP&station=MTC&station=OEB&station=ONZ&station=OSC&station=OZW&station=P53&station=P58&station=P59&station=P75&station=PHN&station=PLN&station=PTK&station=PZQ&station=RMY&station=RNP&station=RQB&station=SAW&station=SJX&station=SLH&station=TEW&station=TTF&station=TVC&station=VLL&station=Y31&station=Y70&station=YIP&data=all&year1='+str(year1)+'&month1='+str(month1)+'&day1='+str(website_date1)+'&year2='+str(year2)+'&month2='+str(month2)+'&day2='+str(website_date2+1)+'&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=no&missing=M&trace=T&direct=no&report_type=3&report_type=4'    #in_url = 'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?data=all&year1=2022&month1=11&day1='+str(website_date1)+'&year2=2022&month2=11&day2='+str(website_date2+1)+'&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=no&missing=M&trace=T&direct=no&report_type=1&report_type=3&report_type=4'
+    in_url = 'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station=1II&station=2R2&station=4I7&station=AID&station=ANQ&station=ASW&station=BAK&station=BFR&station=BMG&station=C62&station=C65&station=CFJ&station=DCY&station=EKM&station=EVV&station=EYE&station=FKR&station=FRH&station=FWA&station=GEZ&station=GGP&station=GPC&station=GSH&station=GUS&station=GWB&station=GYY&station=HBE&station=HFY&station=HHG&station=HLB&station=HNB&station=HUF&station=IMS&station=IND&station=JVY&station=LAF&station=MCX&station=MGC&station=MIE&station=MQJ&station=MZZ&station=OKK&station=OXI&station=PLD&station=PPO&station=RCR&station=RID&station=RZL&station=SBN&station=TYQ&station=UMP&station=UWL&station=VPZ&data=all&year1='+str(year1)+'&month1='+str(month1)+'&day1='+str(website_date1)+'&year2='+str(year2)+'&month2='+str(month2)+'&day2='+str(website_date2+1)+'&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=no&missing=M&trace=T&direct=no&report_type=1&report_type=3&report_type=4'
+    df_parsed1 = pd.read_csv(mi_url)
+    df_parsed2 = pd.read_csv(in_url)
+    df_parsed = pd.concat([df_parsed1, df_parsed2], ignore_index=True)
+
 
     wx_list = df_parsed['wxcodes'].unique()
     snow_list = []
@@ -54,19 +58,23 @@ def Calculate(station_list,date1,time1,date2,time2,wfo):
 
 def Write(local_csv,df2):
     local_csv.loc[-1] = df2
-    local_csv.index = local_csv.index + 1  
-    local_csv = local_csv.sort_index()  
+    local_csv.index = local_csv.index + 1  # shifting index
+    local_csv = local_csv.sort_index()  # sorting by index
     local_csv.to_csv('C:\\Users\\Tony\\Desktop\\Capstone\\farpod.csv',index=False)
 
 def main():
-    date1 = '11/18/2022'
-    time1 = '00:00'
-    date2 = '11/21/2022'
-    time2 = '08:35'
-    wfo = 'grr'
-    station_list = ['LWA', 'AZO', 'BTL', 'RMY', 'FPK', 'Y70', 'GRR', 'RQB' , 'LDM', 'FFX', 'MKG', 'BIV']
+    date1 = '12/17/2022'
+    month1 = '12' 
+    year1 = '2022'
+    time1 = '17:00'
+    date2 = '12/18/2022'
+    month2 = '12' 
+    year2 = '2022'
+    time2 = '00:00'
+    wfo = 'apx_v3'
+    station_list = ['CVX', 'BFA','MGN', 'PLN']
     local_csv = Read()
-    df2 = Calculate(station_list,date1,time1,date2,time2,wfo)
+    df2 = Calculate(station_list,date1,month1,year1,time1,date2,time2,month2,year2,wfo)
     Write(local_csv,df2)
 
 if __name__ == '__main__':
